6ea80dd5ddaa888a8a974ed334764f4030e9ca9c
Subash <subash@Subashs-MacBook-Air.local>
2023-04-15 16:48:17 +0530

    Initial Commit✨

---
+++ src/components/MainTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ Name }}</h2>
+    <p>{{ LongDesc }}</p>
+  </div>
+  <div class="References">
+    <ul>
+      <li>
+        {{ References }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ['Name', 'LongDesc', 'References'],
+};
+</script>
+<style lang="scss"></style>
